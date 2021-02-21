8be0dd1f88c547115f573266d0479cbeaa9eeeb1
zadykian <zadykian@outlook.com>
2021-02-21 16:38:34 +0300

    two-fer

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -1,3 +1,9 @@
 ﻿module TwoFer
 
-let twoFer (input: string option): string = failwith "You need to implement this function."
+let twoFer (input: string option): string =
+    let inputName (): string =
+        match input with
+        | Some name -> name
+        | None -> "you"
+
+    $"One for {inputName()}, one for me."
