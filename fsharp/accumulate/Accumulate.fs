065f1f9d09c58572d9fa04d9abb278b0bc4f8ae3
zadykian <zadykian@outlook.com>
2021-02-22 21:00:55 +0300

    accumulate

--- fsharp/accumulate/Accumulate.fs
+++ fsharp/accumulate/Accumulate.fs
@@ -1,3 +1,10 @@
 ﻿module Accumulate
 
-let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = failwith "You need to implement this function."
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list =
+
+    let rec accumulateImpl (inputList: 'a list) (acc: 'b list): 'b list =
+        match inputList with
+        | head :: tail -> accumulateImpl tail (acc @ [func head])
+        | [] -> acc
+
+    accumulateImpl input []
