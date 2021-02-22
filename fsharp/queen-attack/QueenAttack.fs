03a7f1fd2072eee7957063d19317ea29d7030279
zadykian <zadykian@outlook.com>
2021-02-22 18:42:51 +0300

    queen attack

--- fsharp/queen-attack/QueenAttack.fs
+++ fsharp/queen-attack/QueenAttack.fs
@@ -1,5 +1,15 @@
 ﻿module QueenAttack
 
-let create (position: int * int) = failwith "You need to implement this function."
+let create (position: int * int) =
+    let inRange value = value >= 0 && value <= 7
+    let row, column = position
 
-let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
+    inRange row && inRange column
+
+let canAttack (queen1: int * int) (queen2: int * int) =
+    let firstRow, firstColumn = queen1
+    let secondRow, secondColumn = queen2
+
+    firstRow = secondRow
+    || firstColumn = secondColumn
+    || abs (secondRow - firstRow) = abs (secondColumn - firstColumn) 
