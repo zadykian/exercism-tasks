9d1fbbcfa0b0414f2816f61041ba872f6647edac
zadykian <zadykian@outlook.com>
2021-02-22 20:00:29 +0300

    raindrops

--- fsharp/raindrops/Raindrops.fs
+++ fsharp/raindrops/Raindrops.fs
@@ -1,3 +1,16 @@
 ﻿module Raindrops
 
-let convert (number: int): string = failwith "You need to implement this function."
+open System
+
+let convert (number: int): string =
+    let append (textToAppend: string) (divisor: int) (input: string): string =
+        if number % divisor = 0 then input + textToAppend else input
+
+    let lastStep (input: string): string =
+        if String.IsNullOrWhiteSpace input then number.ToString() else input
+
+    ""
+    |> append "Pling" 3
+    |> append "Plang" 5
+    |> append "Plong" 7
+    |> lastStep
