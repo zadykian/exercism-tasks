6eb7fa084d5cd8baa2348be8de6e2f79668ef889
zadykian <zadykian@outlook.com>
2021-02-23 21:09:24 +0300

    grade-school

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -1,11 +1,26 @@
 ﻿module GradeSchool
 
+open System.Collections.Generic
+
+type Grade = KeyValuePair<int, string list>
 type School = Map<int, string list>
 
-let empty: School = failwith "You need to implement this function."
+let empty: School = Map.empty
 
-let add (student: string) (grade: int) (school: School): School = failwith "You need to implement this function."
+let add (student: string) (grade: int) (school: School): School =    
+    let gradeChangeFunction =
+        function
+        | Some gradeList -> Some (student :: gradeList)
+        | None           -> Some [student]
 
-let roster (school: School): string list = failwith "You need to implement this function."
+    school.Change(grade, gradeChangeFunction)
+let roster (school: School): string list =
+    school
+        |> Seq.sortBy (fun (pair: Grade) -> pair.Key)
+        |> Seq.collect (fun (pair: Grade) -> Seq.sort pair.Value)
+        |> Seq.toList
 
-let grade (number: int) (school: School): string list = failwith "You need to implement this function."
+let grade (number: int) (school: School): string list =
+    match school.TryFind(number) with
+    | Some gradeList -> gradeList
+    | None           -> []
