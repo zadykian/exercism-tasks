1e75981b393b6c962d8ed3748018ae99523ea1d0
zadykian <zadykian@outlook.com>
2021-02-25 23:20:42 +0300

    accumulate

--- fsharp/accumulate/Accumulate.fs
+++ fsharp/accumulate/Accumulate.fs
@@ -4,7 +4,7 @@ let accumulate (func: 'a -> 'b) (input: 'a list): 'b list =
 
     let rec accumulateImpl (inputList: 'a list) (acc: 'b list): 'b list =
         match inputList with
-        | head :: tail -> accumulateImpl tail (acc @ [func head])
+        | head :: tail -> accumulateImpl tail (func head :: acc)
         | [] -> acc
 
-    accumulateImpl input []
+    accumulateImpl input [] |> List.rev
