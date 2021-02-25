f25caefb7f55a8fd9fb26922db6b1a40f21eb308
zadykian <zadykian@outlook.com>
2021-02-25 23:48:38 +0300

    clock

--- fsharp/clock/Clock.fs
+++ fsharp/clock/Clock.fs
@@ -5,12 +5,15 @@ type Clock = { TotalMinutes: uint16 }
 let midnight: Clock = { TotalMinutes = 0us }
 
 let add (minutes: int) (clock: Clock): Clock =
+
     let newTotalMinutes: int = ((int) clock.TotalMinutes) + minutes
 
-    { TotalMinutes =
-          if   newTotalMinutes >= 1440 then (uint16) (newTotalMinutes % 1440)
-          elif newTotalMinutes < 0     then (uint16) (1440 + newTotalMinutes % 1440)
-          else                              (uint16) newTotalMinutes }
+    let normalizedMinutes =
+        if newTotalMinutes >= 0
+        then newTotalMinutes % 1440
+        else 1440 + newTotalMinutes % 1440
+
+    { TotalMinutes = (uint16) normalizedMinutes }
 
 let subtract (minutes: int) = add (-minutes)
 
