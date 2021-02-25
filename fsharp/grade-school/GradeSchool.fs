0070e23dac72a6a45f88ef131b6bd549a921faf3
zadykian <zadykian@outlook.com>
2021-02-25 23:42:14 +0300

    grade-school

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -7,17 +7,26 @@ type School = Map<int, string list>
 
 let empty: School = Map.empty
 
-let add (student: string) (grade: int) (school: School): School =    
+let add (student: string) (grade: int) (school: School): School =
+
+    let addStudentTo (gradeList : string list): string list =
+        (student :: gradeList)
+            |> Seq.sort
+            |> Seq.toList
+
     let gradeChangeFunction =
         function
-        | Some gradeList -> Some (student :: gradeList)
+        | Some gradeList -> Some (addStudentTo gradeList)
         | None           -> Some [student]
 
     school.Change(grade, gradeChangeFunction)
+        |> Seq.sortBy (fun (pair: Grade) -> pair.Key)
+        |> Seq.map (fun key -> (key.Key, key.Value))
+        |> Map.ofSeq
+
 let roster (school: School): string list =
     school
-        |> Seq.sortBy (fun (pair: Grade) -> pair.Key)
-        |> Seq.collect (fun (pair: Grade) -> Seq.sort pair.Value)
+        |> Seq.collect (fun (pair: Grade) -> pair.Value)
         |> Seq.toList
 
 let grade (number: int) (school: School): string list =
