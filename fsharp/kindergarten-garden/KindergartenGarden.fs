39ee259b21bd9495b3fa82a37efedaa0bc1aa186
zadykian <zadykian@outlook.com>
2021-02-28 17:04:34 +0300

    kindergarten garden

--- fsharp/kindergarten-garden/KindergartenGarden.fs
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -1,5 +1,40 @@
 ﻿module KindergartenGarden
 
-// TODO: define the Plant type
+type Plant =
+    | Grass    = 0
+    | Clover   = 1
+    | Radishes = 2
+    | Violets  = 3
 
-let plants diagram student = failwith "You need to implement this function."
+let plants (diagram: string) (student: string) : Plant list =
+    let studentOffset =
+        match student with
+        | "Alice"   -> 00
+        | "Bob"     -> 02
+        | "Charlie" -> 04
+        | "David"   -> 06
+        | "Eve"     -> 08
+        | "Fred"    -> 10
+        | "Ginny"   -> 12
+        | "Harriet" -> 14
+        | "Ileana"  -> 16
+        | "Joseph"  -> 18
+        | "Kincaid" -> 20
+        | "Larry"   -> 22
+        | _         -> invalidArg (nameof student) "Unknown student."
+
+    let charToPlant =
+        function
+        | 'G' -> Plant.Grass
+        | 'C' -> Plant.Clover
+        | 'R' -> Plant.Radishes
+        | 'V' -> Plant.Violets
+        | _   -> failwith "Invalid input char."
+
+    (diagram.Split "\n")
+        |> Array.toSeq
+        |> Seq.toList
+        |> Seq.collect (fun row -> row.[studentOffset .. studentOffset + 1])
+        |> Seq.toList
+        |> Seq.map charToPlant
+        |> Seq.toList
