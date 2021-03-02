7e28b43acd4a80c1ea781663ce4a2111724fd1dc
zadykian <zadykian@outlook.com>
2021-03-02 17:24:14 +0300

    robot-simulator

--- fsharp/robot-simulator/RobotSimulator.fs
+++ fsharp/robot-simulator/RobotSimulator.fs
@@ -11,11 +11,26 @@ type Instruction =
     | TurnLeft
     | Advance
 
+let parseInstruction (inputChar: char): Instruction =
+    match inputChar with
+    | 'R' -> TurnRight
+    | 'L' -> TurnLeft
+    | 'A' -> Advance
+    | _   -> invalidArg (nameof inputChar) "Invalid input character."
+
+let turnRight (direction: Direction): Direction =
+    
+
+let applyToRobot (robot: Robot) (instruction: Instruction): Robot =
+    match instruction with
+    | TurnRight -> 
+
 let create (direction: Direction) (position: Position): Robot =
     {
         direction = direction
         position = position
     }
 
-let move (instructions: string) (robot: Robot): Robot = failwith "You need to implement this function."
-    
+let move (instructions: string) (robot: Robot): Robot =
+    instructions
+    |> Seq.map parseInstruction
