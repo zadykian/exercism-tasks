f141614ce625420fa85a41bd67616b41b6ab33ed
zadykian <zadykian@outlook.com>
2021-03-02 16:56:24 +0300

    robot-simulator

--- fsharp/robot-simulator/RobotSimulator.fs
+++ fsharp/robot-simulator/RobotSimulator.fs
@@ -4,6 +4,6 @@ type Direction = North | East | South | West
 type Position = int * int
 type Robot = { direction: Direction; position: Position }
 
-let create direction position = failwith "You need to implement this function."
+let create (direction: Direction) (position: Position): Robot = failwith "You need to implement this function."
 
-let move instructions robot = failwith "You need to implement this function."
+let move (instructions: string) (robot: Robot): Robot = failwith "You need to implement this function."
