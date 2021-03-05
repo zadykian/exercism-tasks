396e3be31111e5f7cd4e2b4bcf0c6f4ce0c454a7
zadykian <zadykian@outlook.com>
2021-03-05 11:26:29 +0300

    allergies

--- fsharp/allergies/Allergies.fs
+++ fsharp/allergies/Allergies.fs
@@ -2,8 +2,17 @@
 
 open System
 
-// TODO: define the Allergen type
+[<Flags>]
+type Allergen =
+    | Eggs = 1
+    | Peanuts = 2
+    | Shellfish = 4
+    | Strawberries = 8
+    | Tomatoes = 16
+    | Chocolate = 32
+    | Pollen = 64
+    | Cats = 128
 
-let allergicTo codedAllergies allergen = failwith "You need to implement this function."
+let allergicTo (codedAllergies: int) (allergen: Allergen): bool = failwith "You need to implement this function."
 
-let list codedAllergies = failwith "You need to implement this function."
+let list (codedAllergies: int): Allergen list = failwith "You need to implement this function."
