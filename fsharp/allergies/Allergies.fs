d2d378c2b69662297fab9357c076f40d40f9260c
zadykian <zadykian@outlook.com>
2021-03-05 12:30:57 +0300

    allergies

--- fsharp/allergies/Allergies.fs
+++ fsharp/allergies/Allergies.fs
@@ -5,14 +5,14 @@ open System.Reflection
 
 [<Flags>]
 type Allergen =
-    | Eggs = 1
-    | Peanuts = 2
-    | Shellfish = 4
-    | Strawberries = 8
-    | Tomatoes = 16
-    | Chocolate = 32
-    | Pollen = 64
-    | Cats = 128
+    | Eggs         =   1
+    | Peanuts      =   2
+    | Shellfish    =   4
+    | Strawberries =   8
+    | Tomatoes     =  16
+    | Chocolate    =  32
+    | Pollen       =  64
+    | Cats         = 128
 
 let allergicTo (codedAllergies: int) (allergen: Allergen): bool =
      enum<Allergen>(codedAllergies).HasFlag(allergen)
