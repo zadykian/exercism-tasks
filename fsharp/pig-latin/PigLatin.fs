8dfa1ddae1d1e1b33ca5e238f2bff66c3e87021f
zadykian <zadykian@outlook.com>
2021-03-07 20:52:25 +0300

    pig-latin

--- fsharp/pig-latin/PigLatin.fs
+++ fsharp/pig-latin/PigLatin.fs
@@ -1,3 +1,10 @@
 ﻿module PigLatin
 
-let translate (input: string): string = failwith "You need to implement this function."
+type Word = string
+
+let private translateWord (word: Word): Word = failwith "not implemented!"
+
+let translate (input: string): string =
+    input.Split(' ')
+        |> Seq.map translateWord
+        |> String.concat " "
