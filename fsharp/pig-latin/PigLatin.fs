627c3c1f1b60f52e6182c0d84b340715d5ad5053
zadykian <zadykian@outlook.com>
2021-03-07 21:31:10 +0300

    pig-latin

--- fsharp/pig-latin/PigLatin.fs
+++ fsharp/pig-latin/PigLatin.fs
@@ -1,8 +1,36 @@
 ﻿module PigLatin
 
-type Word = string
+type Word =
+    | Translated of string
+    | Untranslated of string
 
-let private translateWord (word: Word): Word = failwith "not implemented!"
+let private translateIfSatisfies
+    (predicate: string -> bool)
+    (translator: string -> string)
+    (word: Word): Word =
+
+    match word with
+    | Translated translated     -> Translated translated
+    | Untranslated untranslated ->
+        if predicate untranslated then Translated (translator untranslated)
+        else Untranslated untranslated
+
+let startsWithVowelSound (word: string): bool =
+    match word |> Seq.toList with
+    | fst :: (snd :: _) -> ['a'; 'e'; 'i'; 'o'; 'u'] |> Seq.contains fst || [fst; snd] = ['x'; 'r']
+    | _                 -> false
+
+let tryGetConsonant (word: string): string option = failwith "not implemented!"
+
+let private translateWord (inputWord: string): string =
+    let handledWord =
+        inputWord
+        |> Untranslated
+        |> translateIfSatisfies startsWithVowelSound (fun w -> $"{w}ay")
+
+    match handledWord with
+    | Translated word   -> word
+    | Untranslated word -> word
 
 let translate (input: string): string =
     input.Split(' ')
