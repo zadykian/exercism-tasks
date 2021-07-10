e5dc78885b7ca54af6d61dcc2f2c19e5f1f46236
zadykian <zadykian@outlook.com>
2021-07-10 14:55:49 +0300

    TreeBuilding

--- fsharp/tree-building/TreeBuilding.fs
+++ fsharp/tree-building/TreeBuilding.fs
@@ -19,54 +19,29 @@ let recordId t =
     | Branch (id, _) -> id
     | Leaf id        -> id
 
-let isBranch (tree: Tree): bool =
-    match tree with
+let isBranch t =
+    match t with
     | Branch _ -> true
-    | Leaf   _ -> false
+    | Leaf _   -> false
 
 let children t =
     match t with
-    | Branch (_, children) -> children
-    | Leaf    _            -> []
+    | Branch (_, c) -> c
+    | Leaf _        -> []
+
+let rec private buildForRoot (records: Record list) (rootId: int): Tree =
+    let children =
+        records
+        |> Seq.filter (fun record -> record.RecordId <> rootId && record.ParentId = rootId)
+        |> Seq.map (fun record -> buildForRoot records record.RecordId)
+        |> Seq.toList
+
+    if Seq.isEmpty children
+    then Leaf rootId
+    else Branch (rootId, children)
 
 let buildTree (records: Record list): Tree =
-    let records' = List.sortBy (fun x -> x.RecordId) records
-
-    if List.isEmpty records' then failwith "Empty input"
-    else
-        let root = records'.[0]
-        if (root.ParentId = 0 |> not) then
-            failwith "Root node is invalid"
-        else
-            if (root.RecordId = 0 |> not) then failwith "Root node is invalid"
-            else
-                let mutable prev = -1
-                let mutable leafs = []
-
-                for r in records' do
-                    if (r.RecordId <> 0 && (r.ParentId > r.RecordId || r.ParentId = r.RecordId)) then
-                        failwith "Nodes with invalid parents"
-                    else
-                        if r.RecordId <> prev + 1 then
-                            failwith "Non-continuous list"
-                        else
-                            prev <- r.RecordId
-                            if (r.RecordId = 0) then
-                                leafs <- leafs @ [(-1, r.RecordId)]
-                            else
-                                leafs <- leafs @ [(r.ParentId, r.RecordId)]
-
-                let root = leafs.[0]
-
-                let grouped = leafs |> List.groupBy fst |> List.map (fun (x, y) -> (x, List.map snd y))
-                let parents = List.map fst grouped
-                let map = grouped |> Map.ofSeq
-
-                let rec helper key =
-                    if Map.containsKey key map then
-                        Branch (key, List.map (fun i -> helper i) (Map.find key map))
-                    else
-                        Leaf key
-
-                let root = helper 0
-                root
+    let rootOption = List.tryFind (fun record -> record.RecordId = record.ParentId) records
+    match rootOption with
+    | Some root -> buildForRoot records root.RecordId
+    | None      -> invalidArg (nameof records) "records list does not contain root!"
