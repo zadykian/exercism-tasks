04b02914ab980f0e340b5da4254f459e0e056b75
zadykian <zadykian@outlook.com>
2021-07-31 15:32:23 +0300

    SumOfMultiples

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -3,16 +3,14 @@
 open System
 
 let sum (numbers: int list) (upperBound: int): int =
-    let multiplesOf (number: int): seq<int>
-        = seq {
+    let multiplesOf (number: int): seq<int> =
+        seq {
             if number = 0 then yield 0
             else for multiplier in 1..Int32.MaxValue -> number * multiplier;
         }
+        |> Seq.takeWhile ((>) upperBound)
 
-    let multiples
-        = numbers
+    numbers
             |> Seq.collect multiplesOf
-            |> Seq.takeWhile ((>) upperBound)
-            |> Seq.toArray
-
-    multiples |> Seq.sum
+            |> Seq.distinct
+            |> Seq.sum
