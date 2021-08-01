535a84be973bebd822cc36ec23fb80dd88c7cb82
zadykian <zadykian@outlook.com>
2021-08-01 17:04:22 +0300

    BinarySearchTree

--- fsharp/binary-search-tree/BinarySearchTree.fs
+++ fsharp/binary-search-tree/BinarySearchTree.fs
@@ -3,16 +3,16 @@
 /// Representation of binary search tree.
 type Tree<'T> when 'T :> System.IComparable<'T> =
     {
-        Value: 'T
+        Data: 'T
         Left:  Tree<'T> option
         Right: Tree<'T> option
     }
 
-let left (node: Tree<'T>): Tree<'T> option = failwith "You need to implement this function."
+let left (node: Tree<'T>): Tree<'T> option = node.Left
 
-let right (node: Tree<'T>): Tree<'T> option = failwith "You need to implement this function."
+let right (node: Tree<'T>): Tree<'T> option = node.Right
 
-let data (node: Tree<'T>): 'T = failwith "You need to implement this function."
+let data (node: Tree<'T>): 'T = node.Data
 
 let create (items: 'T list): Tree<'T> = failwith "You need to implement this function."
 
