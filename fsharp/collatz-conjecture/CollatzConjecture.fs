8ba222a9d78ddafcaf5cb104ec6e2d4863edae4a
zadykian <zadykian@outlook.com>
2021-08-01 15:44:24 +0300

    CollatzConjecture

--- fsharp/collatz-conjecture/CollatzConjecture.fs
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -1,3 +1,12 @@
 ﻿module CollatzConjecture
 
-let steps (number: int): int option = failwith "You need to implement this function."
+let rec private stepsWithCounter (number: uint) (stepsCounter: uint): uint =
+    match number with
+    | 1u                      -> stepsCounter
+    | _ when number % 2u = 0u -> stepsWithCounter (number / 2u) (stepsCounter + 1u)
+    | _                       -> stepsWithCounter (3u * number + 1u) (stepsCounter + 1u)
+
+let steps (number: int): int option =
+    match number with
+    | _ when number > 0 -> Some ((stepsWithCounter (uint number) 0u) |> int)
+    | _                 -> None
