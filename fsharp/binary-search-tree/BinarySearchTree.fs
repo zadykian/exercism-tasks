bebf839c88c12a4044ed32d9176249e817d1ddfd
zadykian <zadykian@outlook.com>
2021-08-01 17:02:11 +0300

    BinarySearchTree: tree type and functions signatures

--- fsharp/binary-search-tree/BinarySearchTree.fs
+++ fsharp/binary-search-tree/BinarySearchTree.fs
@@ -1,11 +1,19 @@
 ﻿module BinarySearchTree
 
-let left node  = failwith "You need to implement this function."
+/// Representation of binary search tree.
+type Tree<'T> when 'T :> System.IComparable<'T> =
+    {
+        Value: 'T
+        Left:  Tree<'T> option
+        Right: Tree<'T> option
+    }
 
-let right node = failwith "You need to implement this function."
+let left (node: Tree<'T>): Tree<'T> option = failwith "You need to implement this function."
 
-let data node = failwith "You need to implement this function."
+let right (node: Tree<'T>): Tree<'T> option = failwith "You need to implement this function."
 
-let create items = failwith "You need to implement this function."
+let data (node: Tree<'T>): 'T = failwith "You need to implement this function."
 
-let sortedData node = failwith "You need to implement this function."
+let create (items: 'T list): Tree<'T> = failwith "You need to implement this function."
+
+let sortedData (node: Tree<'T>): 'T list = failwith "You need to implement this function."
