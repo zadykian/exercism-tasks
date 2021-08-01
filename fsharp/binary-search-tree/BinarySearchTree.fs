c589458667cf8c7abdfa38e9cddd5e70b65b747d
zadykian <zadykian@outlook.com>
2021-08-01 23:13:53 +0300

    BinarySearchTree

--- fsharp/binary-search-tree/BinarySearchTree.fs
+++ fsharp/binary-search-tree/BinarySearchTree.fs
@@ -14,12 +14,27 @@ let right (node: Tree<'T>): Tree<'T> option = node.Right
 
 let data (node: Tree<'T>): 'T = node.Data
 
-/// Add item to tree
-let private add (tree: Tree<'T>) (item: 'T) : Tree<'T> = failwith "not implemented!"
+/// Create single-node tree from item.
+let private fromItem (item: 'T): Tree<'T> = { Data = item; Left = None; Right = None }
 
+/// Add item to tree.
+let rec private add (tree: Tree<'T>) (item: 'T) : Tree<'T> =
+
+    /// Handle tree's subtree by either calling add function recursively
+    /// or creating new node. 
+    let handleSubtree (subtreeOption: Tree<'T> option): Tree<'T> option =
+        match subtreeOption with
+        | Some subtree -> Some (add subtree item)
+        | None         -> Some (fromItem item)
+
+    if tree.Data.CompareTo item >= 0
+    then { tree with Left  = handleSubtree tree.Left  }
+    else { tree with Right = handleSubtree tree.Right }
+
+/// Create tree from list of elements.
 let create (items: 'T list): Tree<'T> =
     match items with
-    | head :: tail -> tail |> Seq.fold add { Data = head; Left = None; Right = None }
+    | head :: tail -> tail |> Seq.fold add (fromItem head)
     | _            -> invalidArg (nameof items) "list cannot be empty!"
 
 let sortedData (node: Tree<'T>): 'T list = failwith "You need to implement this function."
