332cbf649af9c79d4fb967fbb276562794fec7ca
zadykian <zadykian@outlook.com>
2021-08-01 16:42:35 +0300

    ArmstrongNumbers.fs

--- fsharp/armstrong-numbers/ArmstrongNumbers.fs
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -1,3 +1,24 @@
 ﻿module ArmstrongNumbers
 
-let isArmstrongNumber (number: int): bool = failwith "You need to implement this function."
+open System
+
+let private digits (number: int): byte list =
+    let lsd (num: int): byte = byte (num % 10)
+    
+    let rec getWithAcc (num: int) (acc: byte list) =
+        if num >= 10 then getWithAcc (num / 10) (lsd num :: acc)
+        else (lsd num :: acc)
+
+    getWithAcc number []
+
+let isArmstrongNumber (number: int): bool =
+    let digits = digits number
+
+    let summed =
+        digits
+        |> List.toSeq
+        |> Seq.map int
+        |> Seq.map (fun num -> pown num digits.Length)
+        |> Seq.sum
+
+    summed = number
