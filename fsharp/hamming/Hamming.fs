5ba0f17a663bec292682b703a1cf6a22b9b3705a
zadykian <zadykian@outlook.com>
2021-08-02 20:26:44 +0300

    Hamming

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,3 +1,13 @@
 ﻿module Hamming
 
-let distance (strand1: string) (strand2: string): int option = failwith "You need to implement this function."
+let inline private (@) func x = func x
+
+let private distanceNonEmpty (strand1: string) (strand2: string): int =
+    failwith "not implemented!"
+
+let distance (strand1: string) (strand2: string): int option =
+    match (strand1, strand2) with
+    | "", "" -> Some 0
+    | _ , "" -> None
+    | "", _  -> None
+    | _ , _  -> Some @ distanceNonEmpty strand1 strand2
