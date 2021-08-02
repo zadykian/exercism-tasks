f9267f00fc17d252b96bc1cf32d174448325853d
zadykian <zadykian@outlook.com>
2021-08-02 19:50:06 +0300

    Grains

--- fsharp/grains/Grains.fs
+++ fsharp/grains/Grains.fs
@@ -1,4 +1,13 @@
 ﻿module Grains
 
-let square (n: int): Result<uint64,string> = failwith "You need to implement this function."
+let private ifValidDo (n: int) (func: int -> uint64): Result<uint64,string> =
+    if (n >= 1 && n <= 64)
+    then Ok (func n)
+    else Error "square must be between 1 and 64"
+
+let square (n: int): Result<uint64,string> =
+    if (n >= 1 && n <= 64)
+    then Ok 0UL
+    else Error "square must be between 1 and 64"
+
 let total: Result<uint64,string> = failwith "You need to implement this function."
