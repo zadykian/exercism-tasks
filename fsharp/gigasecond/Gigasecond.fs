672e88bd1ac33a57f60610eade5d58f60ce1c848
zadykian <zadykian@outlook.com>
2021-08-02 19:30:35 +0300

    Gigasecond

--- fsharp/gigasecond/Gigasecond.fs
+++ fsharp/gigasecond/Gigasecond.fs
@@ -1,3 +1,9 @@
 ﻿module Gigasecond
 
-let add beginDate = failwith "You need to implement this function."
+open System
+
+/// Add one gigasecond to {beginDate}
+let add (beginDate: DateTime): DateTime =
+    let gigasecond = 1_000_000_000ul
+    let timeSpan = TimeSpan.FromSeconds (float gigasecond)
+    beginDate + timeSpan
