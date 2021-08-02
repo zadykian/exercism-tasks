5f48ecbc753ae5973122185a0d3e45e5d632d9f4
zadykian <zadykian@outlook.com>
2021-08-02 19:22:55 +0300

    DifferenceOfSquares

--- fsharp/difference-of-squares/DifferenceOfSquares.fs
+++ fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -4,18 +4,21 @@
 let private naturalNumbers (count: int): int seq =
     seq { for num in 1..count do yield num }
 
+/// Get square of a {number}.
+let inline private square number = pown number 2
+
 /// Get square of sum of first {number} natural numbers.
 let squareOfSum (number: int): int =
     number
     |> naturalNumbers
     |> Seq.sum
-    |> (fun num -> pown num 2)
+    |> square
 
 /// Get sum of squares of first {number} natural numbers.
 let sumOfSquares (number: int): int =
     number
     |> naturalNumbers
-    |> Seq.map (fun num -> pown num 2)
+    |> Seq.map square
     |> Seq.sum
 
 /// Get difference between square of sum and sum of squares
