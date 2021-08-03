6b324c6e74dd868964dae504b1f2d5e87132fd33
zadykian <zadykian@outlook.com>
2021-08-03 18:44:20 +0300

    Triangle

--- fsharp/triangle/Triangle.fs
+++ fsharp/triangle/Triangle.fs
@@ -1,7 +1,24 @@
 ﻿module Triangle
 
-let equilateral triangle = failwith "You need to implement this function."
+let private tryGetSides triangle =
+    match triangle with
+    | [fst; snd; trd]
+        when (fst + snd > trd
+            && snd + trd > fst
+            && trd + fst > snd) -> Some (fst, snd, trd)
+    | _ -> None
 
-let isosceles triangle = failwith "You need to implement this function."
+let equilateral (triangle: float list) =
+    match tryGetSides triangle with
+    | Some (fst, snd, trd) -> fst = snd && snd = trd
+    | None                 -> false
 
-let scalene triangle = failwith "You need to implement this function."
+let isosceles (triangle: float list) =
+    match tryGetSides triangle with
+    | Some (fst, snd, trd) -> fst = snd || snd = trd || trd = fst
+    | None                 -> false
+
+let scalene (triangle: float list) =
+    match tryGetSides triangle with
+    | Some (fst, snd, trd) -> fst <> snd && snd <> trd && trd <> fst
+    | None                 -> false
