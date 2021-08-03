b60da97869de774e415ab2eb7c651767118ecce5
zadykian <zadykian@outlook.com>
2021-08-03 17:03:33 +0300

    NucleotideCount

--- fsharp/nucleotide-count/NucleotideCount.fs
+++ fsharp/nucleotide-count/NucleotideCount.fs
@@ -1,3 +1,23 @@
 ﻿module NucleotideCount
 
-let nucleotideCounts (strand: string): Option<Map<char, int>> =  failwith "You need to implement this function."
+let inline private (>>=) r f = Option.bind f r
+
+let private rtn v = Some v
+
+let private sequenceList ls =
+    let folder head tail = head >>= (fun h -> tail >>= (fun t -> h::t |> rtn))
+    List.foldBack folder ls (rtn List.empty)
+
+let nucleotideCounts (strand: string): Option<Map<char, int>> =
+
+    let groupToOption (key, chars) =
+        if ['A'; 'T'; 'G'; 'C'] |> Seq.contains key
+        then Some (key, chars |> Seq.length)
+        else None
+
+    strand
+    |> Seq.groupBy id
+    |> Seq.map groupToOption
+    |> Seq.toList
+    |> sequenceList
+    >>= (Map.ofList >> Some)
