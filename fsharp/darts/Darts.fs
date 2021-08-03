77ba0a5e7316fabe2d6a52a0b2b7ae694eecf665
zadykian <zadykian@outlook.com>
2021-08-03 18:20:44 +0300

    Darts

--- fsharp/darts/Darts.fs
+++ fsharp/darts/Darts.fs
@@ -1,3 +1,18 @@
 ﻿module Darts
 
-let score (x: double) (y: double): int = failwith "You need to implement this function."
+let inline private (@) func x = func x
+
+/// Darts board which is represented as 'circle radius to score' map.
+let private board =
+    [
+        (1f , 10)
+        (5f , 5 )
+        (10f, 1 )
+    ]
+
+let score (x: double) (y: double): int =
+    let distance = float32 @ sqrt @ (pown x 2) + (pown y 2)
+    let zone = List.tryFind (fun (radius, _) -> distance <= radius) board
+    match zone with
+    | Some (_, score) -> score
+    | None            -> 0
