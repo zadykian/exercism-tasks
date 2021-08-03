1e15ba4a0c418a46a23b1031fc9173b09d06eb21
zadykian <zadykian@outlook.com>
2021-08-03 18:07:35 +0300

    Etl

--- fsharp/etl/Etl.fs
+++ fsharp/etl/Etl.fs
@@ -1,3 +1,11 @@
 ﻿module Etl
 
-let transform (scoresWithLetters: Map<int, char list>): Map<char, int> = failwith "You need to implement this function."
+open System
+
+let inline private (@) func x = func x
+
+let transform (scoresWithLetters: Map<int, char list>): Map<char, int> =
+    scoresWithLetters
+    |> Map.toSeq
+    |> Seq.collect @ fun (score, chars) -> chars |> Seq.map @ fun char -> (Char.ToLower char, score)
+    |> Map.ofSeq
