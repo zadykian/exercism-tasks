e00be02c52e71f3d8ecdd005053a5ea145644fee
zadykian <zadykian@outlook.com>
2021-08-04 19:12:20 +0300

    RobotName

--- fsharp/robot-name/RobotName.fs
+++ fsharp/robot-name/RobotName.fs
@@ -4,7 +4,6 @@ type RobotName = string
 
 type Robot = { Name: RobotName }
 
-// see: https://stackoverflow.com/questions/2706500/how-do-i-generate-a-random-int-number
 let private random = System.Random ()
 
 let private generateName (): RobotName =
