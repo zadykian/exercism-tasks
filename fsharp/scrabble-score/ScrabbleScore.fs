bf617a3ae08a5a679930b6a180cbbd3e690e8d97
zadykian <zadykian@outlook.com>
2021-08-04 21:04:30 +0300

    ScrabbleScore

--- fsharp/scrabble-score/ScrabbleScore.fs
+++ fsharp/scrabble-score/ScrabbleScore.fs
@@ -1,10 +1,26 @@
 ﻿module ScrabbleScore
 
-let private letterScores: Map<Set<char>, byte> = failwith ""
+let inline private (@) func x = func x
 
-let private getScore (char: char): byte = failwith ""
+let private letterScores: (Set<char> * byte) list =
+    seq {
+        yield ['a'; 'e'; 'i'; 'o'; 'u'; 'l'; 'n'; 'r'; 's'; 't'], 1uy
+        yield ['d'; 'g']                                        , 2uy
+        yield ['b'; 'c'; 'm'; 'p']                              , 3uy
+        yield ['f'; 'h'; 'v'; 'w'; 'y']                         , 4uy
+        yield ['k']                                             , 5uy
+        yield ['j'; 'x']                                        , 8uy
+        yield ['q'; 'z']                                        , 10uy
+    }
+    |> Seq.map @ fun (chars, score) -> (chars |> Set, score)
+    |> Seq.toList
 
-let score (word: string): uint =
+let private getScore (char: char): byte =
+    let lower = char |> System.Char.ToLower
+    let _, score = letterScores |> List.find (fun (chars, _) -> chars |> Set.contains lower)
+    score
+
+let score (word: string): int =
     word
-    |> Seq.map (getScore >> uint)
+    |> Seq.map (getScore >> int)
     |> Seq.sum
