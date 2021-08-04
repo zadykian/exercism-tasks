6b05be605dbca89b57d186e618793e9e1a11ee7a
zadykian <zadykian@outlook.com>
2021-08-04 17:36:39 +0300

    RobotName.fs

--- fsharp/robot-name/RobotName.fs
+++ fsharp/robot-name/RobotName.fs
@@ -4,7 +4,13 @@ type RobotName = string
 
 type Robot = { Name: RobotName }
 
-let private generateName (): RobotName = failwith "not implemented!"
+let private random = System.Random ()
+
+let private generateName (): RobotName =
+    let chars = seq { for char in 'A'..'Z' -> char } |> Seq.toArray
+    let randomChar () = chars.[random.Next chars.Length]
+    let randomDigit () = random.Next 10
+    $"{randomChar ()}{randomChar ()}{randomDigit ()}{randomDigit ()}{randomDigit ()}"
 
 let mkRobot (): Robot = { Name = generateName () }
 
