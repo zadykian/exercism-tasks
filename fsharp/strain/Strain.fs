549e8e85464c477524981f07cf02000160b18d13
zadykian <zadykian@outlook.com>
2021-08-07 19:14:52 +0300

    Strain

--- fsharp/strain/Strain.fs
+++ fsharp/strain/Strain.fs
@@ -1,5 +1,13 @@
 ﻿module Seq
 
-let keep (pred: 'T -> bool) (xs: 'T seq): 'T seq = failwith "You need to implement this function."
+let keep (pred: 'T -> bool) (xs: 'T seq): 'T seq =
+    seq {
+        for item in xs do
+            if pred item then yield item else ()
+    }
 
-let discard (pred: 'T -> bool) (xs: 'T seq): 'T seq = failwith "You need to implement this function."
+let discard (pred: 'T -> bool) (xs: 'T seq): 'T seq =
+    seq {
+        for item in xs do
+            if (pred >> not) item then yield item else ()
+    }
