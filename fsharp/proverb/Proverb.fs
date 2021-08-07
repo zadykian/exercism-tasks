92b4dd0a98dfb0f41ef5af48b5eed787b0bfd544
zadykian <zadykian@outlook.com>
2021-08-07 18:51:05 +0300

    Proverb

--- fsharp/proverb/Proverb.fs
+++ fsharp/proverb/Proverb.fs
@@ -1,3 +1,24 @@
 ﻿module Proverb
 
-let recite (input: string list): string list = failwith "You need to implement this function."
+let inline private uncurry func (a, b) = func a b
+
+let inline private (@) func x = func x
+
+let private singleSentence (cause: string) (consequence:string) =
+    $"For want of a {cause} the {consequence} was lost."
+
+let private final (cause: string) = $"And all for the want of a {cause}."
+
+let recite (input: string list): string list =
+    let causeAndConsequences =
+        input
+        |> Seq.pairwise
+        |> Seq.map @ uncurry singleSentence
+
+    seq {
+        yield! causeAndConsequences
+        match input with
+        | head :: _ -> yield final head
+        | _         -> ()
+    }
+    |> Seq.toList
