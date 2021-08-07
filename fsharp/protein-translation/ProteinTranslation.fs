bc66c8d7e90e5d3d8775109d0b62b59a5672cd27
zadykian <zadykian@outlook.com>
2021-08-07 19:07:35 +0300

    ProteinTranslation

--- fsharp/protein-translation/ProteinTranslation.fs
+++ fsharp/protein-translation/ProteinTranslation.fs
@@ -1,3 +1,37 @@
 ﻿module ProteinTranslation
 
-let proteins rna = failwith "You need to implement this function."
+let inline private (@) func x = func x
+
+/// Codon - sequence of three characters.
+type Codon = string
+
+/// Amino acid which is represented by its' name.
+type AminoAcid = string
+
+let private codonsToAcids =
+    [
+        ["AUG"]                     , "Methionine"
+        ["UUU"; "UUC"]              , "Phenylalanine"
+        ["UUA"; "UUG"]              , "Leucine"
+        ["UCU"; "UCC"; "UCA"; "UCG"], "Serine"
+        ["UAU"; "UAC"]              , "Tyrosine"
+        ["UGU"; "UGC"]              , "Cysteine"
+        ["UGG"]                     , "Tryptophan"
+    ]
+
+let private toAminoAcid (codon: Codon): AminoAcid =
+    codonsToAcids
+    |> List.find @ fun (codons, _) -> codons |> List.contains codon
+    |> snd
+
+let private isStopCodon (codon: Codon): bool =
+    ["UAA"; "UAG"; "UGA"]
+    |> List.contains codon
+
+let proteins rna =
+    rna
+    |> Seq.chunkBySize 3
+    |> Seq.map @ Codon
+    |> Seq.takeWhile (isStopCodon >> not)
+    |> Seq.map toAminoAcid
+    |> Seq.toList
