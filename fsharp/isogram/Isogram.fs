ef290cc93d0fc9426dba1e680b46ee70cda2a6fc
zadykian <zadykian@outlook.com>
2021-08-08 21:02:18 +0300

    Isogram

--- fsharp/isogram/Isogram.fs
+++ fsharp/isogram/Isogram.fs
@@ -1,3 +1,11 @@
 ﻿module Isogram
 
-let isIsogram str = failwith "You need to implement this function."
+open System
+
+let inline private (@) func x = func x
+
+let isIsogram (str: string): bool =
+    str.ToLower ()
+    |> Seq.groupBy id
+    |> Seq.filter @ fun (key, _) -> (Char.IsWhiteSpace >> not) key && key <> '-'
+    |> Seq.forall @ fun (_, chs) -> chs |> Seq.length = 1
