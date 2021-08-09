a7ad99c242636674a9f6cbaf5db369783f866072
zadykian <zadykian@outlook.com>
2021-08-09 17:38:07 +0300

    PhoneNumber

--- fsharp/phone-number/PhoneNumber.fs
+++ fsharp/phone-number/PhoneNumber.fs
@@ -1,3 +1,3 @@
 ﻿module PhoneNumber
 
-let clean input = failwith "You need to implement this function."
+let clean (input: string): Result<uint64, string> = failwith "You need to implement this function."
