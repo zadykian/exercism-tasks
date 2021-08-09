9fc8037d1b732c77193533359ca7437cbf86af92
zadykian <zadykian@outlook.com>
2021-08-09 17:23:51 +0300

    Sieve

--- fsharp/sieve/Sieve.fs
+++ fsharp/sieve/Sieve.fs
@@ -1,3 +1,13 @@
 ﻿module Sieve
 
-let primes limit = failwith "You need to implement this function."
+let rec private primesIn (range: int seq): int seq =
+    seq {
+        for head in Seq.tryHead range |> Option.toList do 
+        yield head
+        yield! range |> Seq.filter (fun num -> num % head <> 0) |> primesIn
+    }
+
+let primes limit =
+    seq { for i in [2..limit] -> i }
+    |> primesIn
+    |> Seq.toList
