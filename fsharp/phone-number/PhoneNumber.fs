2d01730ab15315456023a9605003257848e13466
zadykian <zadykian@outlook.com>
2021-08-11 20:35:55 +0300

    PhoneNumber

--- fsharp/phone-number/PhoneNumber.fs
+++ fsharp/phone-number/PhoneNumber.fs
@@ -1,3 +1,15 @@
 ﻿module PhoneNumber
 
-let clean (input: string): Result<uint64, string> = failwith "You need to implement this function."
+open FParsec
+
+/// String parser without user state.
+type private StrParser = Parser<string, unit>
+
+let private countryCodeParser : StrParser = pstring "+1" <|> pstring "1" <|> pstring ""
+
+let private parser : Parser<uint64, unit> = failwith "not implemented!"
+
+let clean (input: string): Result<uint64, string> =
+    match run parser input with
+    | Success (num, _, _) -> Result.Ok    num
+    | Failure (msg, _, _) -> Result.Error msg
