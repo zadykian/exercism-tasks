38410571c7a6f4b2f5c0e66b895e0e7a4b856063
zadykian <zadykian@outlook.com>
2021-08-17 23:21:46 +0300

    SecretHandshake

--- fsharp/secret-handshake/SecretHandshake.fs
+++ fsharp/secret-handshake/SecretHandshake.fs
@@ -1,3 +1,27 @@
 ﻿module SecretHandshake
 
-let commands number = failwith "You need to implement this function."
+open System.Collections.Specialized
+
+let private events =
+    [
+        0b1   , "wink"
+        0b10  , "double blink"
+        0b100 , "close your eyes"
+        0b1000, "jump"
+    ]
+
+let private commandsUnsafe (number: byte) =
+    let vector = BitVector32(int number)
+
+    let selected =
+        events
+        |> Seq.filter (fun (mask, _) -> vector.[mask])
+        |> Seq.map snd
+
+    let reverse = vector.[0b10000]
+    if reverse then selected |> Seq.rev else selected
+
+let commands number =
+    if   number < 1 || number > 31
+    then []
+    else number |> byte |> commandsUnsafe |> Seq.toList
