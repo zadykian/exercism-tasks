6bf32f9eb9281f7c7326cf5791ca06e9c445ca9d
zadykian <zadykian@outlook.com>
2021-08-17 21:58:15 +0300

    PerfectNumbers

--- fsharp/perfect-numbers/PerfectNumbers.fs
+++ fsharp/perfect-numbers/PerfectNumbers.fs
@@ -2,4 +2,20 @@
 
 type Classification = Perfect | Abundant | Deficient 
 
-let classify n : Classification option = failwith "You need to implement this function."
+let private getPrimeFactors (number: uint): uint seq = failwith "not implemented!"
+
+let private classifyUnsafe (number: uint): Classification =
+    let aliquotSum =
+        number
+        |> getPrimeFactors
+        |> Seq.filter ((<>) number)
+        |> Seq.sum
+
+    if aliquotSum = number   then Perfect
+    elif aliquotSum > number then Abundant
+    else Deficient
+
+let classify number : Classification option =
+    if number > 0
+    then number |> uint |> classifyUnsafe |> Some
+    else None
