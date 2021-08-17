8b0f7450cb502e4ac91939c34a9213d1086b79b4
zadykian <zadykian@outlook.com>
2021-08-17 22:45:28 +0300

    BinarySearch

--- fsharp/binary-search/BinarySearch.fs
+++ fsharp/binary-search/BinarySearch.fs
@@ -4,19 +4,19 @@ type Index = int
 
 let rec find (input: int array) (value: int) : Index option =
 
-    let findUnsafe =
+    let findUnsafe () =
         let index = input.Length / 2
         let middleValue = input.[index]
 
         if middleValue < value then
-            if   index >= input.Length then None
+            if   index + 1 >= input.Length then None
             else find input.[index + 1..] value
 
         elif middleValue > value then
             if   index = 0 then None
             else find input.[..index - 1] value
 
-        else Some value
+        else Some index
 
     if   input.Length = 0 then None
-    else findUnsafe
+    else findUnsafe ()
