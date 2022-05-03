d0d351abc6787acd2f12ceadb4c5cabf90b416cd
zadykian <zadykian@outlook.com>
2022-05-03 20:03:38 +0300

    AffineCipher

--- fsharp/affine-cipher/AffineCipher.fs
+++ fsharp/affine-cipher/AffineCipher.fs
@@ -4,23 +4,45 @@ open System
 
 let inline private (@) func x = func x
 
-let private alphabetSize = 26
+let private letters = ['a' .. 'z'] |> List.toArray
 
 let private failIfNotCoPrime a =
 
     let isCoPrime: bool =
         let rec gcd x y = if y = 0 then x else gcd y (x % y)
 
-        let aToSize = gcd a alphabetSize = 1
-        let sizeToA = gcd alphabetSize a = 1
+        let aToSize = gcd a letters.Length = 1
+        let sizeToA = gcd letters.Length a = 1
         aToSize && sizeToA
 
     if not isCoPrime
-    then raise @ invalidArg (nameof a) $"'{nameof a}' value is not mutually prime with alphabet size ({alphabetSize})"
+    then raise @ invalidArg (nameof a) $"'{nameof a}' value is not mutually prime with alphabet size ({letters.Length})"
 
-let private letters = ['a' .. 'z'] |> List.toArray
+let private mmi (a: int) (n: int) =
+    let rec loop t r newt newr =
+        match newr with
+        | 0 -> if r > 1 then failwith "a is not invertible"
+               else
+                   if t < 0 then t + n
+                   else t
+        | _ -> loop newt newr (t - (r / newr) * newt) (r - (r / newr) * newr)
+    loop 0 n 1 a
 
-let private decodeChar a b char = failwith "not implemented!"
+let private decodeChar a b char =
+
+    let encodedCharIndex () = Array.findIndex ((=) char) letters
+
+    let decodedDefault () = ((mmi a letters.Length) * (encodedCharIndex () - b)) % letters.Length
+
+    let decodedCharIndex () =
+
+        match encodedCharIndex () with
+        | i when i > b -> decodedDefault ()
+        | _ -> decodedDefault () + letters.Length
+
+    if Char.IsDigit char
+    then char
+    else letters[decodedCharIndex ()]
 
 let decode (a: int) (b: int) (cipheredText: string) : string =
     failIfNotCoPrime a
@@ -30,12 +52,12 @@ let decode (a: int) (b: int) (cipheredText: string) : string =
     |> String.map (decodeChar a b)
 
 let private encodeChar (a: int) (b: int) (char: char): char =
-    let charIndex () = Array.findIndex ((=) char) letters
-    let encodedIndex () = (a * charIndex () + b) % alphabetSize
+    let plainCharIndex () = Array.findIndex ((=) char) letters
+    let encodedCharIndex () = (a * plainCharIndex () + b) % letters.Length
 
     if Char.IsDigit char
     then char
-    else letters[encodedIndex ()]
+    else letters[encodedCharIndex ()]
 
 let encode (a: int) (b: int) (plainText: string): string =
     failIfNotCoPrime a
